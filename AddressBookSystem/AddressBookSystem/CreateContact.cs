10600f3696b72104fcd11a589aa1f425db8e336a
roshni1015 <roshniadatrao1110@gmail.com>
2022-04-27 15:05:13 +0530

    Create Contacts

---
+++ AddressBookSystem/AddressBookSystem/CreateContact.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class CreateContact
+    {
+        List<PersonContacts> Addcontacts = new List<PersonContacts>();
+        public void personcontacts()
+        {
+            PersonContacts person = new PersonContacts();
+
+            Console.Write("Enter First Name: ");
+            person.FirstName = Console.ReadLine();
+
+            Console.Write("Enter Last Name: ");
+            person.LastName = Console.ReadLine();
+
+            Console.Write("Enter Address: ");
+            person.LastName = Console.ReadLine();
+
+            Console.Write("Enter City: ");
+            person.LastName = Console.ReadLine();
+
+            Console.Write("Enter State: ");
+            person.LastName = Console.ReadLine();
+
+            Console.Write("Enter Zip: ");
+            person.LastName = Console.ReadLine();
+
+            Console.Write("Enter Phone Number: ");
+            person.PhoneNumber = Console.ReadLine();
+
+            Console.Write("Enter Email: ");
+            person.LastName = Console.ReadLine();
+            Addcontacts.Add(person);
+            Display();
+
+
+        }
+        public void Display()
+        {
+            foreach(var data in Addcontacts)
+            {
+              Console.WriteLine("Enter First Name: " + data.FirstName);
+              Console.WriteLine("Enter Last Name: " + data.LastName);
+              Console.WriteLine("Enter Address: " + data.Address);
+              Console.WriteLine("Enter City: " + data.City);
+              Console.WriteLine("Enter State: " + data.State);
+              Console.WriteLine("Enter Zip: " + data.Zip);
+              Console.WriteLine("Enter Phone Number: " + data.PhoneNumber);
+              Console.WriteLine("Enter Email: " + data.Email);
+            }
+        }
+    }
+}
--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AddressBookSystem;
+
 Console.WriteLine("Welcome to AddressBook System!");
+CreateContact createContact = new CreateContact();
+//createContact.personcontacts();
+//createContact.Display();
+while (true)
+{
+    Console.WriteLine("Enter the option : \n1)Adding Contact \n2)Display contact");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            createContact.personcontacts();
+            break;
+        case 2:
+            createContact.Display();
+            break;
+        default:
+            Console.WriteLine("Please choose correct option");
+            break;
+    }
+
+}
+
